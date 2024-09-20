107a96adbf552d5b536d39ccd155563362c32d57
coolleave <cpy2855048477@163.com>
2024-09-20 23:59:52 +0800

    使用PageHelper插件对员工分页查询进行重构

--- tlias/src/main/java/com/cpy/tlias/mapper/EmpMapper.java
+++ tlias/src/main/java/com/cpy/tlias/mapper/EmpMapper.java
@@ -5,21 +5,27 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper
 public interface EmpMapper {
+//
+//    // 查询记录总数
+//    @Select("select count(*) from emp;")
+//    long empCount();
+//
+//    /**
+//     * page 页码
+//     * pageSize 页码容量
+//     *
+//     */
+//    @Select("select * from emp limit #{start},#{pageSize}")
+//    List<Emp> empList(Integer start ,Integer pageSize);
 
-    // 查询记录总数
-    @Select("select count(*) from emp;")
-    long empCount();
+    // 使用PageHelper 对以上代码进行实现
 
-    /**
-     * page 页码
-     * pageSize 页码容量
-     *
-     */
-    @Select("select * from emp limit #{start},#{pageSize}")
-    List<Emp> empList(Integer start ,Integer pageSize);
+    @Select("select * from emp")
+    List<Emp> empList();
 
 }
--- tlias/src/main/java/com/cpy/tlias/service/Impl/EmpServiceImpl.java
+++ tlias/src/main/java/com/cpy/tlias/service/Impl/EmpServiceImpl.java
@@ -4,6 +4,8 @@ import com.cpy.tlias.mapper.EmpMapper;
 import com.cpy.tlias.polo.Emp;
 import com.cpy.tlias.polo.PageBean;
 import com.cpy.tlias.service.EmpService;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -22,15 +24,29 @@ public class EmpServiceImpl implements EmpService {
      * @param pageSize 页码容量
      * @return 返回PageBean对象给EmpController
      */
-    public PageBean empList (Integer page, Integer pageSize) {
-        // 调用empMapper.empCount()方法查询总数
-        long total = empMapper.empCount();
-        // 计算出起始索引  公式为页码-1 再乘页面容量
-        Integer start = (page-1) * pageSize;
-        // 调用empMapper.empList方法进行员工分页查询
-        List<Emp> empList = empMapper.empList(start,pageSize);
-        // 创建一个PageBean实例，并返回
-        return new PageBean(total,empList);
+//    public PageBean empList (Integer page, Integer pageSize) {
+//        // 调用empMapper.empCount()方法查询总数
+//        long total = empMapper.empCount();
+//        // 计算出起始索引  公式为页码-1 再乘页面容量
+//        Integer start = (page-1) * pageSize;
+//        // 调用empMapper.empList方法进行员工分页查询
+//        List<Emp> empList = empMapper.empList(start,pageSize);
+//        // 创建一个PageBean实例，并返回
+//        return new PageBean(total,empList);
+//
+//    }
+
+    // 使用PageHelper对上述代码进行实现
+    public PageBean empList(Integer page, Integer pageSize){
+        // 1.设置参数
+        PageHelper.startPage(page,pageSize);
+        // 2.执行查询
+        List<Emp> empList = empMapper.empList();
+        // 3.进行强制类型转换  列表->Page
+        Page<Emp> p = (Page<Emp>)empList;
+        // 进行封装
+        return new PageBean(p.getTotal(),p.getResult());
+
 
     }
 }
