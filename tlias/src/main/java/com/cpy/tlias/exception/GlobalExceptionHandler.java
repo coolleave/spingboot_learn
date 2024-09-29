ffea671efb2a62c256cd489647b4debfaad26ab4
coolleave <cpy2855048477@163.com>
2024-09-29 19:14:35 +0800

    完善异常处理，捕获全局异常

---
+++ tlias/src/main/java/com/cpy/tlias/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.cpy.tlias.exception;
+
+import com.cpy.tlias.polo.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice  // 此注解等于@RestControllerAdvice=@ControllerAdvice + @ResponseBody
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)  // 捕获所有的异常
+    public Result ex(Exception ex){
+        ex.printStackTrace();
+        return Result.error("操作失败，请联系管理员");
+    }
+}
